09f104c050aac30fa7995b16a07375bd43b833f7
DMDCstudio <78859148+DMDCstudio@users.noreply.github.com>
2021-02-10 13:46:23 -0500

    Add files via upload

---
+++ mobileNavBar.js
@@ -0,0 +1,12 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+  function openNav() {
+    document.getElementById("myNav").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+  }
+  
